8b29539fb52074011ab5853c62e1d6749be340ab
Franco <francorulos08@gmail.com>
2020-02-27 03:07:28 -0300

    Updates

---
+++ precache-manifest.0f9585056eaa91c3d87feb2318070646.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fe08ce28bd5efbcd1dcfc15987cd2c9",
+    "url": "/Robofriends2020/index.html"
+  },
+  {
+    "revision": "e92a477d9e84f60b89cc",
+    "url": "/Robofriends2020/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "c193775c52c13b5eb6c4",
+    "url": "/Robofriends2020/static/css/main.67804f47.chunk.css"
+  },
+  {
+    "revision": "e92a477d9e84f60b89cc",
+    "url": "/Robofriends2020/static/js/2.a8dbdf4a.chunk.js"
+  },
+  {
+    "revision": "c193775c52c13b5eb6c4",
+    "url": "/Robofriends2020/static/js/main.828d033b.chunk.js"
+  },
+  {
+    "revision": "86f115ad1d153e5c08d1",
+    "url": "/Robofriends2020/static/js/runtime-main.a53e5be4.js"
+  },
+  {
+    "revision": "7cc222a3be796abb86c6308b5862ff59",
+    "url": "/Robofriends2020/static/media/Urba.7cc222a3.woff"
+  }
+]);
